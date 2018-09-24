f9ad6069f0ccaf38d2f47ec06caff7fd0ae13786
Victor Aquiles <victoraqalencar@gmail.com>
2018-09-24 14:00:43 -0300

    Ordenando tambem deputados

--- src/Ordenacao/QuickSort.java
+++ src/Ordenacao/QuickSort.java
@@ -1,10 +1,23 @@
 package Ordenacao;
 
+import GastoDeputados.Deputado;
+
 public class QuickSort extends Ordenacao{
     
     public QuickSort() {}
     
     public void ordenar(Integer[] array){
+        long inicio, fim;
+        
+        inicio = System.currentTimeMillis();
+        
+        this.auxQuickSort(array, 0, array.length -1);
+        
+        fim = System.currentTimeMillis();
+        super.setDuracao(fim - inicio);
+    }
+    
+    public void ordenar(Deputado[] array){
         long inicio, fim;
         
         inicio = System.currentTimeMillis();
@@ -28,10 +41,53 @@ public class QuickSort extends Ordenacao{
         while(i < j) {
             // Percorre o vetor enquanto i < pivo e j > pivo
             // ind_pivo != i para evitar casos em que os valores são iguais
-            while (array[i] <= pivo && ind_pivo != i)
+            while (array[i] <= pivo && ind_pivo != i) {
                 i++;
-            while (array[j] >= pivo && ind_pivo != j)
+                super.compara();
+            }
+            while (array[j] >= pivo && ind_pivo != j){
                 j--;
+                super.compara();
+            }
+            
+            this.troca(array, i, j);
+            
+            // Atualizando o indice do pivo
+            if (ind_pivo == i)
+                ind_pivo = j;
+            else
+                ind_pivo = i;
+        }
+        
+        if (inicio < j) {
+            this.auxQuickSort(array, inicio, j-1);
+        }
+        if (i < fim) {
+            this.auxQuickSort(array, i+1, fim);
+        }
+    }
+    
+    private void auxQuickSort(Deputado[] array, int inicio, int fim) {
+        int pivo, i, j;
+        
+        pivo = array[inicio].deputy_id;
+        int ind_pivo = inicio;
+        i = inicio;
+        j = fim;
+        
+        if (inicio >= fim) return;
+        
+        while(i < j) {
+            // Percorre o vetor enquanto i < pivo e j > pivo
+            // ind_pivo != i para evitar casos em que os valores são iguais
+            while (array[i].deputy_id <= pivo && ind_pivo != i) {
+                i++;
+                super.compara();
+            }
+            while (array[j].deputy_id >= pivo && ind_pivo != j){
+                j--;
+                super.compara();
+            }
             
             this.troca(array, i, j);
             
