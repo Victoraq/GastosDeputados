e3b2a6412f6a378101c31c100c1b69c1da16c943
Unknown <gabi.cesar.w@hotmail.com>
2018-10-01 15:14:55 -0300

    BubbleSort

---
+++ src/Ordenacao/BubbleSort.java
@@ -0,0 +1,26 @@
+package Ordenacao;
+
+public class BubbleSort extends Ordenacao{
+    
+    public BubbleSort() {}
+    
+    public void ordenar(Integer[] array){
+        boolean trocou = true;
+        int tam = array.length;
+        int atual, proximo;
+        
+        while(trocou) {
+            trocou = false;
+            for (int i = 0; i < tam - 1; i++) {
+                
+                atual = array[i];
+                proximo = array[i+1];
+                
+                if (atual > proximo) {
+                    troca(array, i, i+1);
+                    trocou = true;
+                }
+            }
+        }
+    }
+}
