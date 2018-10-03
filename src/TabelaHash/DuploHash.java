519ccd3a6a48a0de211a59335eecfe7b9c607596
Victor Aquiles <victoraqalencar@gmail.com>
2018-10-03 15:40:04 -0300

    Generalizando para mais tipos de dados a tabela hashing
    Além de calculo de uso de memória

--- src/TabelaHash/DuploHash.java
+++ src/TabelaHash/DuploHash.java
@@ -31,24 +31,43 @@ public class DuploHash extends THash{
     /**
      * Metodo que faz a insercao de objetos na tabela.
      * @param dep - objeto da classe Deputado a ser inserido na tabela.
+     * @param coluna - campo que irá servir de chave para a inserção
      */
-    public void inserir(Deputado dep) {
-        int k;
+    public void inserir(Deputado dep, String coluna) {
+        if (null != coluna) // Inserindo por id do deputado se a sua coluna for passada ou for uma string vazia.
+        switch (coluna) {
+            case "deputy_id":
+            case " ":
+                inserirAux(dep, dep.getDeputy_id());
+                break;
+            case "receipt_value":
+                inserirAux(dep, (int) dep.getReceipt_value());
+                break;
+            case "political_party":
+                inserirAux(dep, super.stringToNum(dep.getPolitical_party()));
+                break;
+            default:
+                break;
+        }
+    }
+    
+    /**
+     * Metodo que faz a insercao de objetos na tabela.
+     * @param dep - objeto da classe Deputado a ser inserido na tabela.
+     * @param k - valor chave para inserção
+     */
+    private void inserirAux(Deputado dep, Integer k) {
         
         // Verifica se ha local vazio na tabela, caso contrario não realiza a insercao.
         if(this.numPosPreenchidas >= this.m){
             System.out.println("Tabela cheia!");
             System.out.println(dep);
         }else{
-            try{
-                k = dep.getDeputy_id();
-            }catch(Exception ex) {
-                // Evita a inserção de valor null.
-                // Se acontecer, imprime a mensagem e retorna.
-                System.out.println(ex.getMessage());
-                System.out.println(dep);
+            
+            if (k == null) {
+                System.out.println("Valor nulo");
                 return;
-            }    
+            } 
             
             int numColisoes = 0; // Auxiliar para contagem de colisoes 
             int pos = this.hashDuplo(k, numColisoes); // Funcao de hash duplo
--- src/TabelaHash/ECoalescido.java
+++ src/TabelaHash/ECoalescido.java
@@ -31,9 +31,32 @@ public class ECoalescido extends THash{
     /**
      * Metodo que faz a insercao de objetos na tabela.
      * @param dep - objeto da classe Deputado a ser inserido na tabela.
+     * @param coluna - campo que irá servir de chave para a inserção
      */
-    public void inserir(Deputado dep) {
-        int k;
+    public void inserir(Deputado dep, String coluna) {
+        if (null != coluna) // Inserindo por id do deputado se a sua coluna for passada ou for uma string vazia.
+        switch (coluna) {
+            case "deputy_id":
+            case " ":
+                inserirAux(dep, dep.getDeputy_id());
+                break;
+            case "receipt_value":
+                inserirAux(dep, (int) dep.getReceipt_value());
+                break;
+            case "political_party":
+                inserirAux(dep, super.stringToNum(dep.getPolitical_party()));
+                break;
+            default:
+                break;
+        }
+    }
+    
+    /**
+     * Metodo que faz a insercao de objetos na tabela.
+     * @param dep - objeto da classe Deputado a ser inserido na tabela.
+     * @param k - valor chave para inserção
+     */
+    private void inserirAux(Deputado dep, Integer k) {
         
         // Verifica se ha local vazio na tabela, caso contrario não realiza a insercao.
         if(this.numPosPreenchidas >= this.m){
@@ -41,15 +64,10 @@ public class ECoalescido extends THash{
             System.out.println(dep);
         }else{
             
-            try{
-                k = dep.getDeputy_id();
-            }catch(Exception ex) {
-                // Evita a inserção de valor null.
-                // Se acontecer, imprime a mensagem e retorna.
-                System.out.println(ex.getMessage());
-                System.out.println(dep);
+            if (k == null) {
+                System.out.println("Valor nulo");
                 return;
-            }    
+            } 
             
             int pos = super.hashDivisao(k); // Funcao de hash de divisao
 
