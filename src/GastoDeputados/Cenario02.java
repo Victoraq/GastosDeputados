7494ded88cd9fed2954c0556cb9de557ac7060fd
Unknown <gabi.cesar.w@hotmail.com>
2018-10-06 18:54:23 -0300

    Cenário02 protótipo

--- src/GastoDeputados/Cenario02.java
+++ src/GastoDeputados/Cenario02.java
@@ -19,17 +19,17 @@ public class Cenario02 {
     private Deputado [] dep;
     private Integer []vet ;
     int n;
-    private double []duracao;
+    private long []duracao;
     private double []comparacao;
     private double [] copia;
     public  Cenario02(){
-        dep=new LeituraDados("/home/victor/Documentos/ED2/Java/data/deputies_dataset_tratado.csv",3014904).getDeputados();
+        dep=new LeituraDados("C:\\Users\\gabic\\Downloads\\Engenharia computacional\\ED2\\GastosDeputados\\deputies_dataset_tratado.csv",500000).getDeputados();
     }
     public Integer[] getVet() {
         return vet;
     }
 
-    public double []getDuracao() {
+    public long []getDuracao() {
         return duracao;
     }
 
@@ -43,45 +43,87 @@ public class Cenario02 {
     
     public void preencheVetor_random(){
         Random rand = new Random(System.currentTimeMillis());
+        
         for(int i=0; i<n;i++){
-            vet[i]=dep[Math.abs(rand.nextInt())%(dep.length-1)].getDeputy_id(); 
+
+            int ind=Math.abs(rand.nextInt())%(dep.length-1);
+            try{
+               vet[i]=dep[ind].getDeputy_id(); 
+            } catch(NullPointerException e) {
+                System.out.println(e.getMessage());
+                return;
+            }  
+            System.out.println("vet[i]:"+vet[i]);
         }      
     }
     public void sementeDif( int n){
         this.n=n;
+        vet=new Integer [n];
         for(int i=1;i<=5;i++){
+            System.out.println("vai imprmir semente");
             preencheVetor_random();
+            System.out.println("passou random");
             testaQuickSort();
+            System.out.println("passou testa quicksort");
             int m=10;
             for(int j=0;j<2;j++){
                testaQuickSortInsertion(m);               
                m=100; 
             }
 
-            testaQuickSortMed();  
+            testaQuickSortMed();
+              
         }
     }
     public void testaQuickSort(){
         Integer [] vetor=vet;
+        System.out.println("vetor:"+vetor[0]);
         QuickSort quick=new QuickSort();
         quick.ordenar(vetor);
+ 
+
+       
         duracao[0]=quick.getDuracao();
+        System.out.println("Duração: "+duracao[0]);        
         comparacao[0]=quick.getNumComparacoes();
+        System.out.println("Comparacao: "+comparacao[0]);
         copia[0]=quick.getNumCopias();
+        System.out.println("Copias: "+copia[0]);
     }
     public void testaQuickSortInsertion(int m){
         Integer [] vetor=vet;
         QuickSortInsercao quick=new QuickSortInsercao();
-        quick.ordenar(vetor,m); 
-        duracao[1]=quick.getDuracao();
+        try {
+            quick.ordenar(vetor,m);
+        } catch(NullPointerException e) {
+            System.out.println(e.getMessage());
+            return;
+        }    
+        try{
+            duracao[1]=quick.getDuracao();
+        }catch(NullPointerException e) {
+            System.out.println(e.getMessage());
+            return;
+        }    
         comparacao[1]=quick.getNumComparacoes();
         copia[1]=quick.getNumCopias();        
     }
     public void testaQuickSortMed(){
         Integer [] vetor=vet;
         QuickSortMed quick=new QuickSortMed(vetor);
-        quick.ordenar(0,n-1);
-        duracao[2]=quick.getDuracao();
+        try{
+            quick.ordenar(0,n-1);
+        } catch(NullPointerException e) {
+            System.out.println(e.getMessage());
+            return;
+        }   
+        System.out.println("duração: "+ quick.getDuracao());
+        try{
+            duracao[2]=quick.getDuracao();
+        }catch(NullPointerException e) {
+            System.out.println(e.getMessage());
+            return;
+        }    
         comparacao[2]=quick.getNumComparacoes();
         copia[2]=quick.getNumCopias();        
         //k=3 e k=5
@@ -90,9 +132,9 @@ public class Cenario02 {
     }
 
     public static void main(String[] args) throws IOException {
-        File saida1_int = new File("/home/victor/Documentos/ED2/Java/data/saidaNormal_int.csv");
-        File saida2_int = new File("/home/victor/Documentos/ED2/Java/data/saidaMed_int.csv");
-        File saida3_int = new File("/home/victor/Documentos/ED2/Java/data/saidaInser_int.csv");
+        File saida1_int = new File("C:\\Users\\gabic\\Downloads\\Engenharia computacional\\ED2\\GastosDeputados\\saidaNormal_int.csv");
+        File saida2_int = new File("C:\\Users\\gabic\\Downloads\\Engenharia computacional\\ED2\\GastosDeputados\\saidaMed_int.csv");
+        File saida3_int = new File("C:\\Users\\gabic\\Downloads\\Engenharia computacional\\ED2\\GastosDeputados\\saidaInser_int.csv");
         FileWriter fw1_int,fw2_int,fw3_int;
         try {
             
@@ -116,11 +158,17 @@ public class Cenario02 {
        for(int i=0;i<6;i++){
            c.sementeDif(n[i]);
            double []vetComp=c.getComparacao();
-           double []vetDura=c.getDuracao();
+           long []vetDura=c.getDuracao();
            double []vetCopias=c.getCopia();
            for(int aux=0;aux<3;aux++){
-                String result;
-               result = Integer.toString(n[i])+ ',' +vetDura[aux] + ',' + vetComp[aux]+ ',' + vetCopias[aux] + '\n';
+               String result;
+               try{
+                  
+                   result = Integer.toString(n[i])+ ',' +vetDura[aux] + ',' + vetComp[aux]+ ',' + vetCopias[aux] + '\n';
+               } catch(NullPointerException e) {
+                    System.out.println(e.getMessage());
+                    return;
+               }    
                 if(aux==0){
                     fw1_int.write(result);
                 }
